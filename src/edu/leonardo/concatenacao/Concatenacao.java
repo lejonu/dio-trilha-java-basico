00716445f649f4ecc5630cd4149234b9ba77c195
Leonardo José Nunes <leonardo.ferax@gmail.com>
2022-08-09 17:08:27 -0300

    Concatenacao

---
+++ src/edu/leonardo/concatenacao/Concatenacao.java
@@ -0,0 +1,11 @@
+package edu.leonardo.concatenacao;
+
+public class Concatenacao {
+  public static void main(String[] args) {
+    // calcula até encontar uma string e passa a concatenar
+    System.out.println(1+1+1+"6"+9+9+9);
+
+    // calcula a prioridade e depois concatena
+    System.out.println("6" + (9+9+9));
+  }
+}
