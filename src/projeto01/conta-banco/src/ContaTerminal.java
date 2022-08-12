d2c900c7a47487af568d954625da9e6411ceb26d
Leonardo José Nunes <leonardo.ferax@gmail.com>
2022-08-12 16:42:38 -0300

    Projeto 01

--- src/projeto01/conta-banco/src/ContaTerminal.java
+++ src/projeto01/conta-banco/src/ContaTerminal.java
@@ -1,5 +1,43 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useDelimiter("\n");
+
+        int numeroConta = 0;
+        Double saldo = 0.0;
+
+        System.out.println("Por favor, digite o número da Conta!");
+
+        try {
+            numeroConta = scanner.nextInt();
+        } catch (Exception e) {
+            System.out.println("Erro no número da conta.");
+            System.out.println("Este campo deve conter apenas números.");
+            scanner.close();
+            return;
+        }
+                    
+        System.out.println("Por favor, digite o nome da Agência.");
+        String numeroAgencia = scanner.next(); 
+
+        System.out.println("Por favor, digite o seu Nome.");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Por favor, digite o seu Saldo.");
+        
+        try {
+            saldo = scanner.nextDouble();
+        } catch (Exception e) {
+            System.out.println("Erro na entrada do saldo.");
+            System.out.println("Por favor use o padrão Norte Americano. ex.: 1234.55 ");
+            scanner.close();
+            return;
+        }
+
+        scanner.close();  
+    
+        System.out.println("Olá " + nomeCliente.concat(", obrigado por criar a conta em nosso banco, sua agência é " + numeroAgencia + ", conta " + numeroConta +  " e seu saldo "+ saldo +" já está disponível para saque."));
+           
     }
 }
